648718f93ff3cf19ae6a598758141607eb632022
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 20:27:10 -0300

    exec 3 concluido

---
+++ Exec3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Informe a temperatura em celsius : ");
+double celsius = double.Parse(Console.ReadLine());
+
+double fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+Console.WriteLine($"A temperatura em fahrenheit é: {fahrenheit:F2}");
+Console.WriteLine($"temperatura em celsius: {celsius:F2}");
+Console.WriteLine("fim da execução do programa");
