4845a9db14d33bba6f8e5289826cb1efd90fddba
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 20:46:45 -0300

    exec6 concluido

---
+++ Exec6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("digite um numero:");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("O número é PAR.");
+}
+else
+{
+    Console.WriteLine("O número é ÍMPAR.");
+}
