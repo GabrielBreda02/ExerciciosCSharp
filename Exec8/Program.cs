13830d9c94eb588c042ef014a5444619746987ea
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 20:58:32 -0300

    exec8 concluido

---
+++ Exec8/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Informe o ano: ");
+int ano = int.Parse(Console.ReadLine());
+
+if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
+{
+    Console.WriteLine("O ano informado é bissexto");
+}
+else
+{
+    Console.WriteLine("O ano informado não é bissexto");
+}
