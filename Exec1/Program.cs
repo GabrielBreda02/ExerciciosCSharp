df4519389037f5fc47d6ee4055e55ba5bd3e777d
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 20:14:10 -0300

    exec 1 concluido

--- Exec1/Program.cs
+++ Exec1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("informe o primeiro número: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.Write("informe o segundo número: ");
+double num2 = double.Parse(Console.ReadLine()); 
+
+double soma = num1 + num2;
+
+Console.WriteLine($"A soma dos números é: {soma}");
+Console.WriteLine($"fim da execução do programa");
