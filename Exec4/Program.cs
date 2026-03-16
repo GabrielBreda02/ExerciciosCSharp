da7c03ffd3d31387d7798837fb88ffed35f89deb
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 20:36:01 -0300

    exec 4 concluido

---
+++ Exec4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Informe o raio do circulo: ");
+double raio = double.Parse(Console.ReadLine());
+
+// 2. Calculando a área (A = π * r²)
+// Usamos Math.PI para pegar o valor exato de Pi (3.14159...)
+// Usamos Math.Pow(raio, 2) para elevar o raio ao quadrado (potência de 2)
+
+double area = Math.PI * Math.Pow(raio, 2);
+
+Console.WriteLine($"area calculada:{area:F2}");
+Console.WriteLine("fim da execução do programa");
