da4e83d29a203acc09d9db1f791cf4703f764ed8
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 20:41:44 -0300

    exec5 concluido

---
+++ Exec5/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade >= 18) 
+{
+    Console.WriteLine("você é maior de idade");
+}
+else
+{
+    Console.WriteLine("você é menor de idade");
+}
