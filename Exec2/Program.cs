cd9bd1899d9833e73a6b1968d81582342ca9f62a
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 20:21:08 -0300

    exec 2 concluido

---
+++ Exec2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("informe o primeiro número: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.Write("informe o segundo número: ");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.Write("informe o terceiro número: ");
+double num3 = double.Parse(Console.ReadLine());
+
+double media = (num1 + num2 + num3) / 3.0;
+
+Console.WriteLine($"A média aritmética dos números é: {media:F2}");
+Console.WriteLine("fim da execução do programa");
