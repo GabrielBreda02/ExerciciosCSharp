670faaff75fbc3697684b3b38e9719aa91231097
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 21:14:58 -0300

    exec10 concluido

---
+++ Exec10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// 1. Solicitando o número
+Console.Write("Informe um número: ");
+double numero = double.Parse(Console.ReadLine());
+
+// 2. Verificando a condição do número
+if (numero > 0)
+{
+    Console.WriteLine("O número é positivo");
+}
+else if (numero < 0)
+{
+    Console.WriteLine("O número é negativo");
+}
+else
+{
+    // Se não é maior que zero nem menor, só pode ser zero
+    Console.WriteLine("O número é zero");
+}
+
+// 3. Finalização
+Console.WriteLine("Fim da execução do programa");
