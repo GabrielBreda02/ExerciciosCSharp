8d6fe2bd72c645fc19f688952f25808768497e6e
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 21:35:44 -0300

    exec14 concluido

---
+++ Exec14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// 1. O Laço de Repetição (for) para contagem regressiva
+// int i = 10   -> Começamos em 10
+// i >= 0       -> Continue enquanto i for maior ou igual a 0
+// i--          -> A cada volta, subtraímos 1 do valor de i
+for (int i = 10; i >= 0; i--)
+{
+    // Usamos Console.Write (sem o Line) para os números ficarem na mesma linha
+    Console.Write(i + " ");
+}
+
+// 2. Finalização (Pulando uma linha antes para não ficar grudado nos números)
+Console.WriteLine("\nFim da execução do programa");
