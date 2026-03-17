3817d770589868bfcd0ce47f5adbf6e81b6b9ab3
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 21:03:08 -0300

    exec9 concluido

---
+++ Exec9/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Informe a nota: ");
+double nota = double.Parse(Console.ReadLine());
+
+if (nota >= 60)
+{
+    Console.WriteLine("Situação: Aprovado");
+}
+else
+{
+    Console.WriteLine("Situação: Reprovado");
+}
