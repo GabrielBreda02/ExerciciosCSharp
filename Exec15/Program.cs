9cdb82ca28d61fc4a58bb969314da7bd56650d02
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 21:39:42 -0300

    exec15 concluido

---
+++ Exec15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine("Números pares:");
+
+// 1. O laço percorre de 1 até 100
+for (int i = 1; i <= 100; i++)
+{
+    // 2. Verificamos se o número atual (i) é par
+    if (i % 2 == 0)
+    {
+        // 3. Imprimimos o número seguido de vírgula ou espaço
+        Console.Write(i + (i < 100 ? ", " : ""));
+    }
+}
+
+// 4. Finalização
+Console.WriteLine("\nFim da execução do programa");
