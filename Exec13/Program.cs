ed9f59e11ec218610f07d4ddc6b28eaa053db92d
GABRIEL <gabrielbreda1980@gmail.com>
2026-03-16 21:31:44 -0300

    exec13 concluido

---
+++ Exec13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// 1. Solicitando o número base da tabuada
+Console.Write("Informe um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Tabuada de {numero}:");
+
+// 2. O Laço de Repetição (for)
+// int i = 1    -> Começamos o contador em 1
+// i <= 10      -> A repetição continua enquanto i for menor ou igual a 10
+// i++          -> A cada volta, somamos +1 ao valor de i
+for (int i = 1; i <= 10; i++)
+{
+    int resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
+
+// 3. Finalização
+Console.WriteLine("Fim da execução do programa");
